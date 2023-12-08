d5819bcdef3b9a556f90b4e0cbaa7d8b9b69d20e
kadir-zeren <k4dzeren@gmail.com>
2023-12-08 17:03:51 +0300

    v4

--- src/components/main/Card.jsx
+++ src/components/main/Card.jsx
@@ -1,16 +1,15 @@
-const Card = (props) => {
-  console.log("ne geliyor", props);
+const Card = ({ title, date, image, desc }) => {
   return (
     <div className="cards">
       <div className="title">
-        <h1>title</h1>
+        <h1>{title}</h1>
       </div>
       <div className="date">
-        <h2>date</h2>
+        <h2>{date}</h2>
       </div>
-      <img src="" alt="" />
+      <img src={image} alt="" />
       <div className="card-over">
-        <p>description</p>
+        <p>{desc}</p>
       </div>
     </div>
   );
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,11 +1,12 @@
 import { data } from "../../helpers/data";
 import Card from "./Card";
+import "./Main.scss";
 const Main = () => {
   console.log(data);
   return (
     <div className="card-container">
       {data.map((item) => (
-        <Card {...item} />
+        <Card key={item.id} {...item} />
       ))}
     </div>
   );
